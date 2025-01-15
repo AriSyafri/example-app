d7964625a9d2ff01a0246489e808f4340f13c455
AriSyafri <diamondThink15@gmail.com>
2025-01-15 23:57:24 +0700

    membuat model untuk data dummy blog dan memperbaiki route web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,64 +34,18 @@ Route::get('/about', function () {
 Route::get('/blog', function () {
 
 
-
-
-$blog_posts = [
-    [
-        "title" => "Judul Post Pertama",
-        "slug" => "judul-post-pertama",
-        "author" => "Ari Syafri",
-        "body" => "
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias similique et, sunt temporibus aperiam a voluptate consequatur. Odio autem totam, similique, cum maxime dolores porro amet corporis at quidem expedita temporibus vel minus animi id facilis delectus corrupti, dolorem pariatur beatae. Vel rerum beatae earum exercitationem nisi numquam eveniet consectetur, quia veritatis iste enim ipsa blanditiis praesentium officia? Ipsa, ex soluta autem quos nostrum illo aut eveniet enim vel, sunt vero quae atque! Earum obcaecati, ea deserunt explicabo maiores iusto!"
-    ],
-    [
-        "title" => "Judul Post Kedua",
-        "slug" =>"judul-post-kedua",
-        "author" => "Ujang Agus",
-        "body" => "
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias similique et, sunt temporibus aperiam a voluptate consequatur. Odio autem totam, similique, cum maxime dolores porro amet corporis at quidem expedita temporibus vel minus animi id facilis delectus corrupti, dolorem pariatur beatae. Vel rerum beatae earum exercitationem nisi numquam eveniet consectetur, quia veritatis iste enim ipsa blanditiis praesentium officia? Ipsa, ex soluta autem quos nostrum illo aut eveniet enim vel, sunt vero quae atque! Earum obcaecati, ea deserunt explicabo maiores iusto!"
-    ],
-];
-
-
     return view('posts', [
         "title" => "Blog",
-        "posts" => $blog_posts
+        "posts" => Post::all()
     ]);
 });
 
 // halaman single post
 Route::get('post/{slug}', function($slug){
 
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Ari Syafri",
-            "body" => "
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias similique et, sunt temporibus aperiam a voluptate consequatur. Odio autem totam, similique, cum maxime dolores porro amet corporis at quidem expedita temporibus vel minus animi id facilis delectus corrupti, dolorem pariatur beatae. Vel rerum beatae earum exercitationem nisi numquam eveniet consectetur, quia veritatis iste enim ipsa blanditiis praesentium officia? Ipsa, ex soluta autem quos nostrum illo aut eveniet enim vel, sunt vero quae atque! Earum obcaecati, ea deserunt explicabo maiores iusto!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" =>"judul-post-kedua",
-            "author" => "Ujang Agus",
-            "body" => "
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias similique et, sunt temporibus aperiam a voluptate consequatur. Odio autem totam, similique, cum maxime dolores porro amet corporis at quidem expedita temporibus vel minus animi id facilis delectus corrupti, dolorem pariatur beatae. Vel rerum beatae earum exercitationem nisi numquam eveniet consectetur, quia veritatis iste enim ipsa blanditiis praesentium officia? Ipsa, ex soluta autem quos nostrum illo aut eveniet enim vel, sunt vero quae atque! Earum obcaecati, ea deserunt explicabo maiores iusto!"
-        ],
-    ];
-
-
-    $new_post = [];
-
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
 
     return view('post', [
         "title" => "Single Post",
-        "post" =>$new_post
+        "post" => Post::find($slug)
     ]);
 });
