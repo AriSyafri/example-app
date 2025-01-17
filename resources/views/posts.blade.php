0c7ed090ac81041decd1d97500f1ab6e117fa27a
AriSyafri <diamondThink15@gmail.com>
2025-01-17 21:23:35 +0700

    membuat post model dari db

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,10 +9,9 @@
     @foreach ($posts as $post)
     <article class="mb-5">
         <h2>
-            <a href="/posts/{{ $post["id"] }}">{{ $post["title"] }}</a>
+            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
         </h2>
-        <h5>{{  $post["author"] }}</h5>
-        <p>{{ $post["body"] }}</p>
+        <p>{{ $post->excerpt }}</p>
     </article>
 
     @endforeach
