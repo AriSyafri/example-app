380906f1514acfd96420849850d8fd55b7caf795
AriSyafri <diamondThink15@gmail.com>
2025-01-22 20:17:09 +0700

    membuat factory post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(5)->create();
+
 
         // User::create([
         //     'name' => 'Ari Syafri',
@@ -37,7 +37,7 @@ class DatabaseSeeder extends Seeder
         //     'password' => bcrypt('12345')
         // ]);
 
-
+        User::factory(5)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -49,60 +49,62 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'judul Kedua',
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
 
 
-        Post::create([
-            'title' => 'judul Ketiga',
-            'slug' => 'judul-ketiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'judul Ketiga',
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'judul Keempat',
-            'slug' => 'judul-keempat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'judul Keempat',
+        //     'slug' => 'judul-keempat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'judul Kelima',
-            'slug' => 'judul-kelima',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
-            'category_id' => 2,
-            'user_id' => 3
-        ]);
+        // Post::create([
+        //     'title' => 'judul Kelima',
+        //     'slug' => 'judul-kelima',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
+        //     'category_id' => 2,
+        //     'user_id' => 3
+        // ]);
 
-        Post::create([
-            'title' => 'judul Keenam',
-            'slug' => 'judul-keenam',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'judul Keenam',
+        //     'slug' => 'judul-keenam',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt officiis nihil laudantium, saepe consectetur tempora dignissimos illum, consequatur tempore provident ducimus modi hic porro aliquam laborum eos facilis nobis? Sed, veritatis unde? Architecto officiis maiores repudiandae. Quia pariatur veritatis qui, numquam amet laborum fuga distinctio in quo eaque aliquam ratione maxime aspernatur modi obcaecati labore et error libero saepe quas. Nostrum, adipisci accusamus! Illum nemo eveniet quae tenetur maiores, eligendi enim, nihil dolorem modi quaerat exercitationem, praesentium eum! Ullam voluptatem cum earum cumque nesciunt sunt quasi facilis facere amet tempora, dolores soluta illum sit maiores dolorum! Dolorem, doloremque saepe! Repellat.',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
 
     }
 }
