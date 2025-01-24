260d7f6c3edced5e332e7d616c907c6e4881fe91
AriSyafri <diamondThink15@gmail.com>
2025-01-24 17:42:17 +0700

    menambah range category id

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -19,7 +19,7 @@ class PostFactory extends Factory
             'excerpt' => $this->faker->paragraph(),
             'body' => $this->faker->paragraph(mt_rand(5,10)),
             'user_id' => mt_rand(1,3),
-            'category_id' => mt_rand(1,2)
+            'category_id' => mt_rand(1,3)
         ];
     }
 }
