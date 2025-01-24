ec1428525d96a343fb653556145728983ca2adfb
AriSyafri <diamondThink15@gmail.com>
2025-01-24 17:12:52 +0700

    menambah fitur author namun masih di route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 
-use App\Models\Category;
 
+use App\Models\Category;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -51,5 +52,12 @@ Route::get('/categories/{category:slug}', function(Category $category) {
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name
+    ]);
+});
+
+Route::get('/authors/{user}', function(User $user) {
+    return view('posts', [
+        'title' => 'User Posts',
+        'posts' => $user->posts,
     ]);
 });
