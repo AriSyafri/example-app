8d27167e8acc95910d9daa0ba7c3b311663a5b78
AriSyafri <diamondThink15@gmail.com>
2025-01-27 23:09:29 +0700

    memperbaiki tampilan authors

--- resources/views/authors.blade.php
+++ resources/views/authors.blade.php
@@ -6,17 +6,32 @@
 
  @section('container')
 
-     <h1 class="mb-5">Author List</h1>
+    <h1 class="mb-5">Author List</h1>
 
-     @foreach ($authors as $author)
+    <div class="container">
+        <div class="row justify-content-center">
 
-     <ul>
-         <li>
-             <h2><a href="/authors/{{ $author->username }}" class="text-decoration-none">{{ $author->name }}</a></h2>
-         </li>
-     </ul>
+            @foreach ($authors as $author)
 
+                <div class="col-md-3">
 
-     @endforeach
+                    <div class="card">
+                        <img src="/img/thumb.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title"><a href="/authors/{{ $author->username }}" class="text-decoration-none">{{ $author->name }}</a></h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><a href="/authors/{{ $author->username }}" class="btn btn-primary text-decoration-none">Go to blog...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            @endforeach
+
+        </div>
+    </div>
+
 
  @endsection
