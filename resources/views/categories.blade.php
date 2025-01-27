6b1660f7296a756c5b3a3ff548c39eadf0083ee8
AriSyafri <diamondThink15@gmail.com>
2025-01-27 16:02:53 +0700

    menambah tampilan categories

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -8,15 +8,30 @@
 
     <h1 class="mb-5">Post Categories</h1>
 
-    @foreach ($categories as $category)
+    <div class="container">
+        <div class="row">
 
-    <ul>
-        <li>
-            <h2><a href="/categories/{{ $category->slug }}" class="text-decoration-none">{{ $category->name }}</a></h2>
-        </li>
-    </ul>
+            @foreach ($categories as $category)
 
 
-    @endforeach
+                <div class="col-md-4">
+
+                    <a href="/categories/{{ $category->slug }}">
+
+                        <div class="card bg-dark text-white">
+                            <img src="/img/thumb.jpg" class="card-img" alt="...">
+                            <div class="card-img-overlay d-flex align-items-center text-center p-0">
+                            <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0,0,0,0.7)">{{ $category->name }}</h5>
+
+                            </div>
+                        </div>
+                    </a>
+
+                </div>
+
+            @endforeach
+
+        </div>
+    </div>
 
 @endsection
