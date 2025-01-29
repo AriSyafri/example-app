38f975da8e71eb263fcdf22a7e49f688ad01f8ca
AriSyafri <diamondThink15@gmail.com>
2025-01-29 11:33:29 +0700

    mengganti route dan pencarina berdasarkan category dan judul yang akan dicari

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,6 +11,11 @@
     <div class="row mb-3 justify-content-center">
         <div class="col-md-6">
             <form action="/posts">
+
+                @if ( request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+
                 <div class="input-group mb-3">
                     <input type="text" class="form-control" placeholder="Search..." name="search" value="{{ request('search') }}">
                     <button class="btn btn-primary" type="submit" >Search</button>
@@ -27,7 +32,7 @@
             <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
             <p>
                 <small class="text-muted">
-                    By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }} </a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+                    By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }} </a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
                 </small>
             </p>
             <p class="card-text">{{ $posts[0]->excerpt }}</p>
@@ -47,12 +52,12 @@
 
             <div class="col-md-4 mb-3">
                 <div class="card">
-                    <div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{  $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
+                    <div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/posts?category={{  $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
                     <img src="/img/thumb.jpg" class="card-img-top" alt="image">
                     <div class="card-body">
                       <h5 class="card-title">{{ $post->title }}</h5>
                         <small class="text-muted">
-                        By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }} </a> {{ $post->created_at->diffForHumans() }}
+                            By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }} </a> {{ $post->created_at->diffForHumans() }}
                         </small>
                       <p class="card-text">{{ $post->excerpt }}</p>
                       <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more..</a>
