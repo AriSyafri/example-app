7fa567286b00f2ff3d51cb944787c3e332e02691
AriSyafri <diamondThink15@gmail.com>
2025-01-31 17:53:53 +0700

    memperbaiki route author

--- resources/views/authors.blade.php
+++ resources/views/authors.blade.php
@@ -18,7 +18,7 @@
                     <div class="card">
                         <img src="/img/thumb.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                        <h5 class="card-title"><a href="/posts?author{{ $author->username }}" class="text-decoration-none">{{ $author->name }}</a></h5>
+                        <h5 class="card-title"><a href="/posts?author={{ $author->username }}" class="text-decoration-none">{{ $author->name }}</a></h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                         <ul class="list-group list-group-flush">
