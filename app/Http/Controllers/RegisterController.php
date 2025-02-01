e8c294c9e1ba97135b0e95b5401870d6d15b5c2e
AriSyafri <diamondThink15@gmail.com>
2025-02-01 12:30:35 +0700

    menggungah register dan redirect menuju login

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Contracts\Support\ValidatedData;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -17,14 +22,23 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'name' => 'required|max:255',
             'username' => ['required', 'min:3', 'max:255', 'unique:users'],
             'email' => 'required|email:dns|unique:users',
             'password' => 'required|min:5|max:255'
         ]);
 
-        dd('registrasi berhasil');
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        $request->session()->flash('success', 'Registration successfull! Please login');
+
+
+
+        return redirect('/login');
     }
 
 }
