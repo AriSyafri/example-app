7f7a585bb92b3d871809d5119ddd1d61e933bce4
AriSyafri <diamondThink15@gmail.com>
2025-02-17 01:10:31 +0700

    menambah gate agar sidebar post category hanya muncul di admin

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -17,18 +17,21 @@
 
       </ul>
 
-      <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1">
-        <span>Administrator</span>
-      </h6>
+      @can('admin')
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1">
+            <span>Administrator</span>
+        </h6>
 
-      <ul class="nav flex-column">
-        <li class="nav-item">
-            <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }} " href="/dashboard/categories">
-                <span data-feather="grid"></span>
-                Post Categories
-            </a>
-        </li>
-      </ul>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }} " href="/dashboard/categories">
+                    <span data-feather="grid"></span>
+                    Post Categories
+                </a>
+            </li>
+        </ul>
+      @endcan
+
 
 
     </div>
